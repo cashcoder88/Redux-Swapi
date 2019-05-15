2c33491d0d72f1dffa53167944aaba0bc11e2480
cashcoder88 <cashglobe@gmail.com>
2019-05-15 15:57:06 -0600

    Finished reducer for now

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
 // we'll need axios
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
